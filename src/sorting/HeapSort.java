d0718f8a000bed31633b53b26e1257e8ade58789
Sam Penny <sp2070@cam.ac.uk>
2022-02-04 17:33:49 +0000

    Added heapsort, quicksort, counting sort and testing classes

---
+++ src/sorting/HeapSort.java
@@ -0,0 +1,43 @@
+package sorting;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class HeapSort implements SortingAlgo {
+    public static void main(String[] args) {
+        SortTesting.test(new QuickSort());
+    }
+
+    public void sort(int[] a) {
+        for (int k = a.length / 2 - 1; k >= 0; k--) {
+            heapify(a, a.length, k);
+        }
+
+        for (int k = a.length; k > 1; k--) {
+            int temp = a[0];
+            a[0] = a[k - 1];
+            a[k - 1] = temp;
+            heapify(a, k - 1, 0);
+        }
+
+    }
+
+    public static void heapify(int[] arr, int iEnd, int iRoot) {
+        int j = iRoot;
+        int l = 2 * iRoot + 1;
+        int r = 2 * iRoot + 2;
+        if (l < iEnd && arr[j] < arr[l]) {
+            j = l;
+        }
+        if (r < iEnd && arr[j] < arr[r]) {
+            j = r;
+        }
+
+        if (j != iRoot) {
+            int tmp = arr[iRoot];
+            arr[iRoot] = arr[j];
+            arr[j] = tmp;
+            heapify(arr, iEnd, j);
+        }
+    }
+}
---
+++ src/sorting/SortingAlgo.java
@@ -0,0 +1,5 @@
+package sorting;
+
+public interface SortingAlgo {
+    void sort(int[] array);
+}
